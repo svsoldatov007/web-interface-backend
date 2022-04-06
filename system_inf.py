8ab77df6af6a08fdc9f75fbd3cd08579a61e8d43
Soldatov Semen <svsoldatov@miem.hse.ru>
2022-04-06 17:31:09 +0300

    MVP - second commit

--- system_inf.py
+++ system_inf.py
@@ -1,5 +1,5 @@
 import os
-# import psutil
+import psutil
 import datetime
 import schedule
 import json
@@ -36,14 +36,13 @@ def get_output_address():
 
 
 def get_device_time():
-    '''command = ' date "+%H:%M:%S   %d/%m/%y"'
+    command = ' date "+%H:%M:%S   %d/%m/%y"'
     result = str(os.popen(command).read()).strip()
-    return result'''
-    return "10:17:16 23/03/22"
+    return result
 
 
 def get_running_time():
-    '''command = "awk '{print $1}' /proc/uptime"
+    command = "awk '{print $1}' /proc/uptime"
     result = int(float(str(os.popen(command).read()).strip()))    # in seconds
     day = result//(3600*24)
     hour = (result-day*3600*24)//3600
@@ -51,51 +50,44 @@ def get_running_time():
     sec = result-day*3600*24-hour*3600-minute*60
     if day < 10:
         day = '0'+str(day)
-    return str(day)+':'+str(time(hour, minute, sec))'''
-    return '00:01:07:48'
+    return str(day)+':'+str(datetime.time(hour, minute, sec))
 
 
 def get_cpu_usage():
-
-    # return int(round(psutil.cpu_percent()))
-    return 20
+    return int(round(psutil.cpu_percent()))
 
 
 def get_cpu_temperature():
 
-    '''command = "cat /sys/class/thermal/thermal_zone0/temp"
-    return int(os.popen(command).read())//1000'''
-
-    return 29
+    command = "cat /sys/class/thermal/thermal_zone0/temp"
+    return int(os.popen(command).read())//1000
 
 
 def get_memory_usage():
-    '''command = 'vmstat -s'
+    command = 'vmstat -s'
     result = str(os.popen(command).read()).strip().split()
     total = round(float(result[0])/1024, 1)
     used = round(float(result[4])/1024, 1)
-    return str(used) + 'M/' + str(total) + 'M'''
+    return str(used) + 'M/' + str(total) + 'M'
 
-    return '1460.3M/3956.1M'
+
 
 
 # DHCP status:
 
 def get_ip():
-    '''result = str(os.popen("ip a").read()).split()
+    result = str(os.popen("ip a").read()).split()
     ip_mask = ''
     for i in range(len(result)):
         if result[i] == 'inet' and '127.0.0.1' not in result[i+1] and '172.17.0.1' not in result[i+1]:
             ip_mask = result[i+1]
             break
     ip = ip_mask[:ip_mask.find('/')]
-    return ip'''
-
-    return '172.18.146.30'
+    return ip
 
 
 def get_netmask():
-    '''result = str(os.popen("ip a").read()).split()
+    result = str(os.popen("ip a").read()).split()
     ip_mask = ''
     for i in range(len(result)):
         if result[i] == 'inet' and '127.0.0.1' not in result[i + 1] and '172.17.0.1' not in result[i + 1]:
@@ -111,26 +103,14 @@ def get_netmask():
     for i in range(4):
         netmask = netmask + str(int(mask_1_0[8 * i:8 * i + 8], base=2)) + '.'
 
-    return netmask[:len(netmask)-1]     # without the last '.'''
+    return netmask[:len(netmask)-1]     # without the last '.'
 
-    return  '255.255.255.0'
 
 
 def get_default_gateway():
-    '''command = "echo $(/sbin/ip route | awk '/default/ { print $3 }')"
+    command = "echo $(/sbin/ip route | awk '/default/ { print $3 }')"
     result = str(os.popen(command).read()).strip()
-    return result'''
-
-    return '172.18.146.1'
+    return result
 
 
 # ssh jetson@172.18.146.30
-
-'''print('device time:', get_device_time())
-print('running time:', get_running_time())
-print('cpu usage in percents:', get_cpu_usage())
-print('cpu temperature =', get_cpu_temperature())
-print('memory usage:', get_memory_usage())
-print('ip:', get_ip())
-print('netmask:', get_netmask())
-print('default gateway:', get_default_gateway())'''
